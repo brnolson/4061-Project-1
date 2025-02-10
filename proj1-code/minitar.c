ef3c1b06524b9448456197140c14125f01d1349c
Michael Fera <mikeyfera1@gmail.com>
2025-02-10 10:37:09 -0600

    Added Archive Create and List functions
    Added the create archive file and listing files from an archive function as well as updating the main_minitar.c file

--- proj1-code/minitar.c
+++ proj1-code/minitar.c
@@ -5,6 +5,7 @@
 #include <math.h>
 #include <pwd.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/sysmacros.h>
@@ -29,14 +30,14 @@
  * standard for tar file structure.
  */
 void compute_checksum(tar_header *header) {
-    // Have to initially set header's checksum to "all blanks"
-    memset(header->chksum, ' ', 8);
-    unsigned sum = 0;
-    char *bytes = (char *) header;
-    for (int i = 0; i < sizeof(tar_header); i++) {
-        sum += bytes[i];
-    }
-    snprintf(header->chksum, 8, "%07o", sum);
+  // Have to initially set header's checksum to "all blanks"
+  memset(header->chksum, ' ', 8);
+  unsigned sum = 0;
+  char *bytes = (char *)header;
+  for (int i = 0; i < sizeof(tar_header); i++) {
+    sum += bytes[i];
+  }
+  snprintf(header->chksum, 8, "%07o", sum);
 }
 
 /*
@@ -45,100 +46,241 @@ void compute_checksum(tar_header *header) {
  * Returns 0 on success or -1 if an error occurs
  */
 int fill_tar_header(tar_header *header, const char *file_name) {
-    memset(header, 0, sizeof(tar_header));
-    char err_msg[MAX_MSG_LEN];
-    struct stat stat_buf;
-    // stat is a system call to inspect file metadata
-    if (stat(file_name, &stat_buf) != 0) {
-        snprintf(err_msg, MAX_MSG_LEN, "Failed to stat file %s", file_name);
-        perror(err_msg);
-        return -1;
-    }
+  memset(header, 0, sizeof(tar_header));
+  char err_msg[MAX_MSG_LEN];
+  struct stat stat_buf;
+  // stat is a system call to inspect file metadata
+  if (stat(file_name, &stat_buf) != 0) {
+    snprintf(err_msg, MAX_MSG_LEN, "Failed to stat file %s", file_name);
+    perror(err_msg);
+    return -1;
+  }
 
-    strncpy(header->name, file_name, 100);    // Name of the file, null-terminated string
-    snprintf(header->mode, 8, "%07o",
-             stat_buf.st_mode & 07777);    // Permissions for file, 0-padded octal
+  strncpy(header->name, file_name,
+          100); // Name of the file, null-terminated string
+  snprintf(header->mode, 8, "%07o",
+           stat_buf.st_mode & 07777); // Permissions for file, 0-padded octal
 
-    snprintf(header->uid, 8, "%07o", stat_buf.st_uid);    // Owner ID of the file, 0-padded octal
-    struct passwd *pwd = getpwuid(stat_buf.st_uid);       // Look up name corresponding to owner ID
-    if (pwd == NULL) {
-        snprintf(err_msg, MAX_MSG_LEN, "Failed to look up owner name of file %s", file_name);
-        perror(err_msg);
-        return -1;
-    }
-    strncpy(header->uname, pwd->pw_name, 32);    // Owner name of the file, null-terminated string
+  snprintf(header->uid, 8, "%07o",
+           stat_buf.st_uid); // Owner ID of the file, 0-padded octal
+  struct passwd *pwd =
+      getpwuid(stat_buf.st_uid); // Look up name corresponding to owner ID
+  if (pwd == NULL) {
+    snprintf(err_msg, MAX_MSG_LEN, "Failed to look up owner name of file %s",
+             file_name);
+    perror(err_msg);
+    return -1;
+  }
+  strncpy(header->uname, pwd->pw_name,
+          32); // Owner name of the file, null-terminated string
 
-    snprintf(header->gid, 8, "%07o", stat_buf.st_gid);    // Group ID of the file, 0-padded octal
-    struct group *grp = getgrgid(stat_buf.st_gid);        // Look up name corresponding to group ID
-    if (grp == NULL) {
-        snprintf(err_msg, MAX_MSG_LEN, "Failed to look up group name of file %s", file_name);
-        perror(err_msg);
-        return -1;
-    }
-    strncpy(header->gname, grp->gr_name, 32);    // Group name of the file, null-terminated string
+  snprintf(header->gid, 8, "%07o",
+           stat_buf.st_gid); // Group ID of the file, 0-padded octal
+  struct group *grp =
+      getgrgid(stat_buf.st_gid); // Look up name corresponding to group ID
+  if (grp == NULL) {
+    snprintf(err_msg, MAX_MSG_LEN, "Failed to look up group name of file %s",
+             file_name);
+    perror(err_msg);
+    return -1;
+  }
+  strncpy(header->gname, grp->gr_name,
+          32); // Group name of the file, null-terminated string
 
-    snprintf(header->size, 12, "%011o",
-             (unsigned) stat_buf.st_size);    // File size, 0-padded octal
-    snprintf(header->mtime, 12, "%011o",
-             (unsigned) stat_buf.st_mtime);    // Modification time, 0-padded octal
-    header->typeflag = REGTYPE;                // File type, always regular file in this project
-    strncpy(header->magic, MAGIC, 6);          // Special, standardized sequence of bytes
-    memcpy(header->version, "00", 2);          // A bit weird, sidesteps null termination
-    snprintf(header->devmajor, 8, "%07o",
-             major(stat_buf.st_dev));    // Major device number, 0-padded octal
-    snprintf(header->devminor, 8, "%07o",
-             minor(stat_buf.st_dev));    // Minor device number, 0-padded octal
+  snprintf(header->size, 12, "%011o",
+           (unsigned)stat_buf.st_size); // File size, 0-padded octal
+  snprintf(header->mtime, 12, "%011o",
+           (unsigned)stat_buf.st_mtime); // Modification time, 0-padded octal
+  header->typeflag = REGTYPE; // File type, always regular file in this project
+  strncpy(header->magic, MAGIC, 6); // Special, standardized sequence of bytes
+  memcpy(header->version, "00", 2); // A bit weird, sidesteps null termination
+  snprintf(header->devmajor, 8, "%07o",
+           major(stat_buf.st_dev)); // Major device number, 0-padded octal
+  snprintf(header->devminor, 8, "%07o",
+           minor(stat_buf.st_dev)); // Minor device number, 0-padded octal
 
-    compute_checksum(header);
-    return 0;
+  compute_checksum(header);
+  return 0;
 }
 
 /*
  * Removes 'nbytes' bytes from the file identified by 'file_name'
  * Returns 0 upon success, -1 upon error
- * Note: This function uses lower-level I/O syscalls (not stdio), which we'll learn about later
+ * Note: This function uses lower-level I/O syscalls (not stdio), which we'll
+ * learn about later
  */
 int remove_trailing_bytes(const char *file_name, size_t nbytes) {
-    char err_msg[MAX_MSG_LEN];
+  char err_msg[MAX_MSG_LEN];
 
-    struct stat stat_buf;
-    if (stat(file_name, &stat_buf) != 0) {
-        snprintf(err_msg, MAX_MSG_LEN, "Failed to stat file %s", file_name);
-        perror(err_msg);
-        return -1;
-    }
+  struct stat stat_buf;
+  if (stat(file_name, &stat_buf) != 0) {
+    snprintf(err_msg, MAX_MSG_LEN, "Failed to stat file %s", file_name);
+    perror(err_msg);
+    return -1;
+  }
 
-    off_t file_size = stat_buf.st_size;
-    if (nbytes > file_size) {
-        file_size = 0;
-    } else {
-        file_size -= nbytes;
-    }
+  off_t file_size = stat_buf.st_size;
+  if (nbytes > file_size) {
+    file_size = 0;
+  } else {
+    file_size -= nbytes;
+  }
 
-    if (truncate(file_name, file_size) != 0) {
-        snprintf(err_msg, MAX_MSG_LEN, "Failed to truncate file %s", file_name);
-        perror(err_msg);
-        return -1;
-    }
-    return 0;
+  if (truncate(file_name, file_size) != 0) {
+    snprintf(err_msg, MAX_MSG_LEN, "Failed to truncate file %s", file_name);
+    perror(err_msg);
+    return -1;
+  }
+  return 0;
 }
 
+/**
+ * Creates an archive file using archive_name and stores the provided list of files
+ * within it using files
+ *
+ * Returns 0 upon success, -1 upon error
+ *
+ * Loops through the linked list of files and using fill_tar_header to make the header
+ * Then it opens the file using open() and reads and writes the contents to the archive file
+ * using a buffer
+ *
+ * Then loops again to make the footer blocks by adding empty blocks at the end of the entire archive file
+ */
 int create_archive(const char *archive_name, const file_list_t *files) {
-    // TODO: Not yet implemented
+  // Creating and Opening the Archive file
+  int archive_file = open(archive_name, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+  if (archive_file < 0) {
+    perror("Error: Unable to open archive file");
+    return -1;
+  }
+
+  // Looping through each file, and creating a header block and content blocks
+  node_t *current = files->head;
+  while (current != NULL) {
+    // Through each file, fill the header by calling fill_tarr_header with the file name
+    tar_header header;
+    if (fill_tar_header(&header, current->name) != 0) {
+      perror("Error: Failed to fill tar header");
+      close(archive_file);
+      return -1;
+    }
+
+    // Write the header to the archive file
+    if (write(archive_file, &header, sizeof(tar_header)) !=
+        sizeof(tar_header)) {
+      perror("Error: Failed to write header to archive");
+      close(archive_file);
+      return -1;
+    }
+
+    // Open the contents of the file after header creation
+    int file_fd = open(current->name, O_RDONLY);
+    if (file_fd < 0) {
+      perror("Error: Failed to open file");
+      close(archive_file);
+      return -1;
+    }
+
+    // Read the contents of the file and write to the archive file
+    char buffer[BLOCK_SIZE] = {0};
+    ssize_t bytes_read;
+    while ((bytes_read = read(file_fd, buffer, BLOCK_SIZE)) > 0) {
+      if (write(archive_file, buffer, BLOCK_SIZE) != BLOCK_SIZE) {
+        perror("Error: Failed to write file contents to archive");
+        close(file_fd);
+        close(archive_file);
+        return -1;
+      }
+      memset(buffer, 0, BLOCK_SIZE);
+    }
+
+    if (bytes_read < 0) {
+      perror("Error: Failed to read file");
+      close(file_fd);
+      close(archive_file);
+      return -1;
+    }
+
+    close(file_fd);
+    current = current->next;
+  }
+
+  // Write trailing blocks to mark end of archive
+    char empty_block[BLOCK_SIZE] = {0};
+    for (int i = 0; i < NUM_TRAILING_BLOCKS; i++) {
+      if (write(archive_file, empty_block, BLOCK_SIZE) != BLOCK_SIZE) {
+        perror("Error: Failed to write trailing blocks");
+        close(archive_file);
+        return -1;
+      }
+    }
+
+    // Removing any unneccessary trailing bytes if possible
+    if (remove_trailing_bytes(archive_name, NUM_TRAILING_BLOCKS * BLOCK_SIZE) != 0) {
+      perror("Error: Failed to remove trailing bytes");
+      close(archive_file);
+      return -1;
+    }
+
+    close(archive_file);
     return 0;
 }
 
-int append_files_to_archive(const char *archive_name, const file_list_t *files) {
+  int append_files_to_archive(const char *archive_name,
+                              const file_list_t *files) {
     // TODO: Not yet implemented
     return 0;
-}
+  }
 
-int get_archive_file_list(const char *archive_name, file_list_t *files) {
-    // TODO: Not yet implemented
-    return 0;
-}
+  /*
+   * Reads an archive file and extracts the list of contained file names
+   *
+   * STILL WORKING ON RIGHT NOW - Mikey Fera
+   */
+  int get_archive_file_list(const char *archive_name, file_list_t *files) {
+    int archive_file = open(archive_name, O_RDONLY);
 
-int extract_files_from_archive(const char *archive_name) {
+    if (archive_file < 0) {
+      perror("Error: Unable to open archive file");
+      return -1;
+    }
+
+    while (1) {
+      tar_header header;
+      ssize_t bytes_read;
+
+      if ((bytes_read = read(archive_file, &header, sizeof(tar_header))) < 0) {
+        perror("Error: Failed to read header block");
+        close(archive_file);
+        return -1;
+      }
+      if (bytes_read == 0 || header.name[0] == '\0') {
+        break;
+      }
+
+      printf("%s\n", header.name);
+
+      if (file_list_add(files, header.name) != 0) {
+        perror("Error: Failed to add file to list");
+        close(archive_file);
+        return -1;
+      }
+
+      // GONNA CHNAGE THIS PART -> finding a different way to calculate each file
+      off_t file_size = strtol(header.size, NULL, 8);
+      off_t padded_size = ((file_size + BLOCK_SIZE -1) / BLOCK_SIZE) * BLOCK_SIZE;
+      if (lseek(archive_file, padded_size, SEEK_CUR) < 0) {
+        perror("error: Failed to seek to next header");
+        close(archive_file);
+        return -1;
+      }
+    }
+
+    // close(archive_file);
+    return 0;
+  }
+
+  int extract_files_from_archive(const char *archive_name) {
     // TODO: Not yet implemented
     return 0;
-}
+  }
--- proj1-code/minitar_main.c
+++ proj1-code/minitar_main.c
@@ -16,6 +16,69 @@ int main(int argc, char **argv) {
     // TODO: Parse command-line arguments and invoke functions from 'minitar.h'
     // to execute archive operations
 
+    const char *archive_name = NULL;
+    int operation = 0;
+
+    // Checking the agruments in the command line to check for each minitat function
+    // Depending on the operation, makes the value operation have a different number
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-c") == 0) {
+            operation = 1;
+        } else if (strcmp(argv[i], "-a") == 0) {
+            operation = 2;
+        } else if (strcmp(argv[i], "-t") == 0) {
+            operation = 3;
+        } else if (strcmp(argv[i], "-u") == 0) {
+            operation = 4;
+        } else if (strcmp(argv[i], "-x") == 0) {
+            operation = 5;
+        } else if (strcmp(argv[i], "-f") == 0 && i + 1 < argc) {
+            archive_name = argv[i + 1];
+            i++;
+        } else {
+            if (file_list_add(&files, argv[i]) != 0) {
+                perror("Error: Failed to add file to list");
+                file_list_clear(&files);
+                return 1;
+            }
+        }
+    }
+
+    // If no real operation was used or archive_name does not exist, report an error
+    if (operation == 0 || archive_name == NULL) {
+        printf("Error: INvalid operation or missing archive name.\n");
+        file_list_clear(&files);
+        return 1;
+    }
+
+    // Used a switch case depending on the operation number and called respected function
+    int result = 0;
+    switch (operation) {
+        case 1:
+            result = create_archive(archive_name, &files);
+            break;
+        case 2:
+            result = append_files_to_archive(archive_name, &files);
+            break;
+        case 3:
+            result = get_archive_file_list(archive_name, &files);
+            break;
+        case 5:
+            result = extract_files_from_archive(archive_name);
+            break;
+        default:
+            printf("Error: Unsupported operation.\n");
+            file_list_clear(&files);
+            return 1;
+    }
+
+    // If the operation failed, report an error
+    if (result != 0) {
+        fprintf(stderr, "Error: Archive creation failed.\n");
+        file_list_clear(&files);
+        return 1;
+    }
+
     file_list_clear(&files);
     return 0;
 }
